9a41e6dc5e9e99760058c4846c0486ee9b2b347d
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2015-12-21 17:00:23 +0000

    Adding Turbine Server

---
+++ core/src/main/java/com/tjazi/infra/turbineserver/core/TurbineServerApplication.java
@@ -0,0 +1,14 @@
+package com.tjazi.infra.turbineserver.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TurbineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineServerApplication.class, args);
+    }
+}

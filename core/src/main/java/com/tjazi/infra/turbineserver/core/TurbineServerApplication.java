bec7a911586652347f627bc7d18683116ba9c3ca
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2015-12-24 17:15:04 +0000

    Introducing @EnableTurbine annotation

--- core/src/main/java/com/tjazi/infra/turbineserver/core/TurbineServerApplication.java
+++ core/src/main/java/com/tjazi/infra/turbineserver/core/TurbineServerApplication.java
@@ -3,9 +3,11 @@ package com.tjazi.infra.turbineserver.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTurbine
 public class TurbineServerApplication {
 
     public static void main(String[] args) {
